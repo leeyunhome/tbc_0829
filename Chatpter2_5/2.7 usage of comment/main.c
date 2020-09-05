abe74fc7d9c524c2c7011e4a108739a4d6266d66
leeyunhome <leeyunhome@daum.net>
2020-09-05 16:04:57 +0900

    add 2.7 usage of comment

---
+++ Chatpter2_5/2.7 usage of comment/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	// variables
+	int x, y, sum;
+
+	x = 1;
+	y = 4;
+
+	sum = x + y;	// add two numbers
+
+	printf("%i + %i = %i", x, y, sum);
+	printf("\a");
+
+	return 0;
+}
