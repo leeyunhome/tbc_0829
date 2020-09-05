6e7c855708d9edc1242ff44c929a88c074ed972d
leeyunhome <leeyunhome@daum.net>
2020-09-05 15:56:08 +0900

    add 2.6 printf function usage

---
+++ Chatpter2_5/2_6 printf function/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	//int a = printf("\"The truth is ... \nI'm IronMan.\n\"");
+	//// escapte sequence
+
+	//printf("%d\n", a);
+	int x, y, z;
+
+	x = 1;
+	y = 2;
+
+	z = x + y;
+
+	printf("%i + %i = %i\n", x, y, z);
+	printf("\a");
+
+	return 0;
+}
