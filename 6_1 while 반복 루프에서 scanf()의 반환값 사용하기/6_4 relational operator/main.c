c60a77c5cd4746da447f143f34e122444314eeb9
leeyunhome <leeyunhome@daum.net>
2020-09-12 19:41:52 +0900

    add 6.4 relational operator

---
+++ 6_1 while 반복 루프에서 scanf()의 반환값 사용하기/6_4 relational operator/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>	// fabs()
+
+int main()
+{
+	/*
+		Relational Operators
+		<		is less than
+		<=		is less than or equal to
+		==		is equal to
+		>=		is greater than or equal to
+		>		is greater than
+		!=		is not equal to
+	*/
+
+	//int n = 0;
+	//while (n++ <5)		// n++ < 5 is a relational expression
+	//{
+	//	printf("%d ", n);
+	//}
+	//printf("\n");
+
+	//char c = 'A';
+	//while (c != 'Z')
+	//	printf("%c ", c++);
+
+	const double PI = 3.1415926535897932384626433832795;
+	double guess = 0.0;
+
+	printf("Input PI : ");
+	scanf("%lf", &guess);;
+	//while (guess != PI)
+	while (fabs(guess - PI) > 0.001)
+	{
+		printf("Fool! Try again.\n");
+		int flag = scanf("%lf", &guess);
+		if (flag != 1)
+		{
+			printf("Wrong input");
+			return;
+		}
+	}
+
+	printf("Good!");
+
+	return 0;
+}
