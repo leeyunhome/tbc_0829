b838731c62f5944a76abc6500da2278654f2f32b
leeyunhome <leeyunhome@daum.net>
2020-09-12 20:10:58 +0900

    add 6.5 Truth and False

---
+++ 6_1 while 반복 루프에서 scanf()의 반환값 사용하기/6.5 사실과 거짓/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//#define TRUE 1
+//#define FALSE 0
+
+int main()
+{
+	/*int tv, fv;
+	tv = (1 < 2);
+	fv = (1 > 2);
+
+	printf("True is %d\n", tv);
+	printf("False is %d\n", fv);*/
+
+	//int i = 5;
+	//while (i)
+	//{
+	//	printf("%d is true\n", i--);
+	//}
+	//printf("%d is false\n", i);
+
+	while (1)
+	{
+
+	}
+	return 0;
+}
