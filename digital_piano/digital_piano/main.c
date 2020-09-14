eb2c003bb7f9d2a733d4c3afdcfc97bfdea6d984
leeyunhome <leeyunhome@daum.net>
2020-09-15 05:31:29 +0900

    by myself

--- digital_piano/digital_piano/main.c
+++ digital_piano/digital_piano/main.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 #include <Windows.h>
+#include "MyHeader.h"
 
 #define NUM_KEYS 3
+
+char key_list[] = { 'a', 's', 'd' };
 float frequency_list[] = { 261.626 , 293.665, 329.628 };
-float key[] = { 'a', 's', 'd' };
+
+float getFrequencyFromKey(char key);
+void playSound(void);
+
 int main()
+{
+	while (1)
+	{
+		playSound();
+	}
+
+	return 0;
+}
+void playSound(void)
 {
 	char ch = 0;
 
 	ch = getchar();
+	Beep(getFrequencyFromKey(ch), 500);
 
-	for (int idx = 0; idx < NUM_KEYS; ++idx)
-	{
-		if (key[idx] == ch)
-		{
-			Beep(frequency_list[idx], 500);
-		}
-	}
+	//for (int idx = 0; idx < NUM_KEYS; ++idx)
+	//{
+	//	if (key_list[idx] == ch)
+	//	{
+	//		Beep(frequency_list[idx], 500);
+	//	}
+	//}
 	//switch (ch)
 	//{
 	//case 'a':
@@ -31,6 +47,14 @@ int main()
 	//default:
 	//	break;
 	//}
-
-	return 0;
+}
+float getFrequencyFromKey(char key)
+{
+	for (int idx = 0; idx < NUM_KEYS; ++idx)
+	{
+		if (key_list[idx] == key)
+		{
+			Beep(frequency_list[idx], 500);
+		}
+	}
 }
