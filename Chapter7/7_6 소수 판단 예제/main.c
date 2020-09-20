2819bf789f3821702a36b88c79825b32a7fbae4f
leeyunhome <leeyunhome@daum.net>
2020-09-21 04:39:07 +0900

    add 7.6 소수판단 예제

--- Chapter7/7_6 소수 판단 예제/main.c
+++ Chapter7/7_6 소수 판단 예제/main.c
@@ -16,10 +16,23 @@ int main()
 	*/
 	
 	isPrime = true;
-	for (div = 2; div < num; ++div)
+	for (div = 2; (div*div) <= num; ++div)
 	{
 		if (num % div == 0)
+		{
 			isPrime = false;
+
+			if (num == div * div)
+			{
+				printf("%u is divisible by %u\n", num, div);
+
+			}
+			else
+			{
+				printf("%u is divisible by %u and %u\n", num, div, num / div);
+			}
+
+		}
 
 	}
 
