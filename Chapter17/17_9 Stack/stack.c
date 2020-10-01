8cc52944a606ed48000f60f7e9e57df9b252e570
leeyunhome <leeyunhome@daum.net>
2020-10-02 05:57:16 +0900

    add 17_9 Stack

--- Chapter17/17_9 Stack/stack.c
+++ Chapter17/17_9 Stack/stack.c
@@ -3,14 +3,13 @@
 
 void Initialize(Stack* stack)
 {
-	stack->items->key = 0;
+	//stack->items->key = 0;
 	stack->top = -1;
 }
 bool IsFull(const Stack* stack)
 {
 	if (stack->top == MAX_STACK_SIZE - 1)
 	{
-		printf("Stack is full. Cannot push.\n");
 		return true;
 	}
 
@@ -32,6 +31,10 @@ void Push(Stack* stack, element item)
 		stack->top++;
 		stack->items[stack->top] = item;
 	}
+	else
+	{
+		printf("Stack is full. Cannot push.\n");
+	}
 	
 }
 element Pop(Stack* stack)
@@ -45,5 +48,7 @@ element Pop(Stack* stack)
 	else
 	{
 		printf("Stack is empty. Cannot remove.\n");
+		element temp = { -1 };// place holder, dummy
+		return temp;
 	}
 }
